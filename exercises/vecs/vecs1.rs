378d0371ce68aece58e0e93bd607f2341736bb1f
ywtong1555 <18157921697@189.cn>
2025-02-05 15:51:03 +0800

    update
    	修改：      exercises/move_semantics/move_semantics4.rs
    	修改：      exercises/move_semantics/move_semantics5.rs
    	修改：      exercises/move_semantics/move_semantics6.rs
    	修改：      exercises/structs/structs1.rs
    	修改：      exercises/vecs/vecs1.rs
    	修改：      exercises/vecs/vecs2.rs

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -7,13 +7,15 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE 20250205
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v = vec![10,20,30,40];// TODO: declare your vector here with the macro for vectors
+    let mut Vec = a ;
 
-    (a, v)
+    (a, v)  
+    
 }
 
 #[cfg(test)]
